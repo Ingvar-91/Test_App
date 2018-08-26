055c01f83f57e35c433592e0b40767865a5debe3
Ingvar-91 <Ingvar-91@mail.ru>
2018-08-26 12:08:03 +0600

    init

---
+++ src/pages/about/about.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-about',
+  templateUrl: 'about.html'
+})
+export class AboutPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+  
+  slides = [
+    {
+      title: "Welcome to the Docs!",
+      description: "The <b>Ionic Component Documentation</b> showcases a number of useful components that are included out of the box with Ionic.",
+      image: "https://tair.shop/img/slider/small/547fd4b8d11ec0118effa965ee.png",
+    },
+    {
+      title: "What is Ionic?",
+      description: "<b>Ionic Framework</b> is an open source SDK that enables developers to build high quality mobile apps with web technologies like HTML, CSS, and JavaScript.",
+      image: "https://tair.shop/img/slider/small/9f6448d75d8b813a801baba9cf.jpg",
+    },
+    {
+      title: "What is Ionic Cloud?",
+      description: "The <b>Ionic Cloud</b> is a cloud platform for managing and scaling Ionic apps with integrated services like push notifications, native builds, user auth, and live updating.",
+      image: "https://tair.shop/img/slider/small/61a92a9eef57aae0e59364acbc.png",
+    }
+];
+
+}
